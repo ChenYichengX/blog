f4566b529096e11dcdd4e510ea1da02066ab23f9
ChenYichengX <361415506@qq.com>
2021-10-22 18:03:45 +0800

    时间时区bug

--- src/main/java/com/chen/blog/interceptor/WebConfig.java
+++ src/main/java/com/chen/blog/interceptor/WebConfig.java
@@ -1,13 +1,21 @@
 package com.chen.blog.interceptor;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * @author 陈奕成
@@ -15,6 +23,28 @@ import java.util.List;
  */
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
+
+//    @Bean
+//    public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
+//        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+//        ObjectMapper mapper = new ObjectMapper();
+//        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",Locale.getDefault()));
+//        mapper.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+//        converter.setObjectMapper(mapper);
+//        return converter;
+//    }
+//
+//    /**
+//     * 添加转换器
+//     * @param converters    converters
+//     */
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        //将我们定义的时间格式转换器添加到转换器列表中,
+//        //这样jackson格式化时候但凡遇到Date类型就会转换成我们定义的格式
+//        converters.add(jackson2HttpMessageConverter());
+//    }
 
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
