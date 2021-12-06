428281c9fe287c76c2e80ceb79c69205766d1593
ChenYichengX <361415506@qq.com>
2021-12-06 15:33:15 +0800

    type、tag 分页bug修复

--- src/main/java/com/chen/blog/web/TagShowController.java
+++ src/main/java/com/chen/blog/web/TagShowController.java
@@ -1,11 +1,14 @@
 package com.chen.blog.web;
 
 import com.chen.blog.aspect.SystemLog;
+import com.chen.blog.entity.Blog;
 import com.chen.blog.entity.Tag;
 import com.chen.blog.service.BlogService;
 import com.chen.blog.service.TagService;
 import com.chen.blog.vo.BlogQuery;
+import com.chen.blog.vo.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,7 +42,36 @@ public class TagShowController {
             id = tags.get(0).getId();
         }
         model.addAttribute("tags",tags);
-        model.addAttribute("page",blogService.listBlog(id,pageable));
+        Page<Blog> blogs = blogService.listBlog(id, pageable);
+        model.addAttribute("page",blogs);
+        List<PageResult> page = new ArrayList<>();
+        if (blogs.getTotalPages() <= 4) {
+            for (int i = 0; i < blogs.getTotalPages(); i++) {
+                if (blogs.getNumber() == i) {
+                    page.add(new PageResult(i + 1, true));
+                } else {
+                    page.add(new PageResult(i + 1, false));
+                }
+            }
+        } else {
+            int number = blogs.getNumber();
+            if (number >= 2) {
+                page.add(new PageResult(number - 1, false));
+                page.add(new PageResult(number, false));
+                page.add(new PageResult(number + 1, true));
+                page.add(new PageResult(number + 2, false));
+                page.add(new PageResult(number + 3, false));
+            } else {
+                for (int i = 0; i <= 4; i++) {
+                    if (number == i) {
+                        page.add(new PageResult(i + 1, true));
+                    } else {
+                        page.add(new PageResult(i + 1, false));
+                    }
+                }
+            }
+        }
+        model.addAttribute("pageSize", page);
         model.addAttribute("activeTagId",id);
         return "tags";
     }
--- src/main/java/com/chen/blog/web/TypeShowController.java
+++ src/main/java/com/chen/blog/web/TypeShowController.java
@@ -1,11 +1,14 @@
 package com.chen.blog.web;
 
 import com.chen.blog.aspect.SystemLog;
+import com.chen.blog.entity.Blog;
 import com.chen.blog.entity.Type;
 import com.chen.blog.service.BlogService;
 import com.chen.blog.service.TypeService;
 import com.chen.blog.vo.BlogQuery;
+import com.chen.blog.vo.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -40,7 +44,36 @@ public class TypeShowController {
         BlogQuery blogQuery = new BlogQuery();
         blogQuery.setTypeId(id);
         model.addAttribute("types",types);
-        model.addAttribute("page",blogService.listBlog(pageable,blogQuery,null));
+        Page<Blog> blogs = blogService.listBlog(pageable, blogQuery, null);
+        model.addAttribute("page",blogs);
+        List<PageResult> page = new ArrayList<>();
+        if (blogs.getTotalPages() <= 4) {
+            for (int i = 0; i < blogs.getTotalPages(); i++) {
+                if (blogs.getNumber() == i) {
+                    page.add(new PageResult(i + 1, true));
+                } else {
+                    page.add(new PageResult(i + 1, false));
+                }
+            }
+        } else {
+            int number = blogs.getNumber();
+            if (number >= 2) {
+                page.add(new PageResult(number - 1, false));
+                page.add(new PageResult(number, false));
+                page.add(new PageResult(number + 1, true));
+                page.add(new PageResult(number + 2, false));
+                page.add(new PageResult(number + 3, false));
+            } else {
+                for (int i = 0; i <= 4; i++) {
+                    if (number == i) {
+                        page.add(new PageResult(i + 1, true));
+                    } else {
+                        page.add(new PageResult(i + 1, false));
+                    }
+                }
+            }
+        }
+        model.addAttribute("pageSize", page);
         model.addAttribute("activeTypeId",id);
         return "type";
     }
